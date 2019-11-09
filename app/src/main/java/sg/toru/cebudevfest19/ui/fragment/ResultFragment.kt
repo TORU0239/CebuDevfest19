e9953b67345a08b2d1376d880819a792822d2fb5
Toru Choi <blueocean239@gmail.com>
2019-11-09 18:02:57 +0800

    add google font, and result fragment

---
+++ app/src/main/java/sg/toru/cebudevfest19/ui/fragment/ResultFragment.kt
@@ -0,0 +1,20 @@
+package sg.toru.cebudevfest19.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import sg.toru.cebudevfest19.R
+
+class ResultFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+}
