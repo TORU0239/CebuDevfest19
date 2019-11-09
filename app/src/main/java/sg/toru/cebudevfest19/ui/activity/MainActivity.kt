28388b40ff1971cd9873faafa9b6b23399ddbcb8
Toru Choi <blueocean239@gmail.com>
2019-11-09 18:44:34 +0800

    add tranparent navigation

--- app/src/main/java/sg/toru/cebudevfest19/ui/activity/MainActivity.kt
+++ app/src/main/java/sg/toru/cebudevfest19/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package sg.toru.cebudevfest19.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
 import sg.toru.cebudevfest19.R
 
@@ -13,4 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         container = findViewById(R.id.fragment_container)
     }
+
+    override fun onResume() {
+        super.onResume()
+        container.postDelayed({
+            container.systemUiVisibility = FLAGS_FULLSCREEN
+        }, 500L)
+    }
 }
+
+const val FLAGS_FULLSCREEN =
+    View.SYSTEM_UI_FLAG_LOW_PROFILE or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
